c7e876bd33031c5cfa17f566aafc0642516a20b1
Greg <gskril@gmail.com>
2022-07-03 23:31:02 -0400

    Install web3 libs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const { chains, provider } = configureChains(
+	[chain.mainnet, chain.rinkeby],
+	[alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+	appName: 'My RainbowKit App',
+	chains,
+})
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+})
+
+const App = ({ Component, pageProps }) => {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiConfig>
+	)
 }
 
-export default MyApp
+export default App
