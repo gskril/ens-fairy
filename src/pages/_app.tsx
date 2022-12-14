fb48aa99f4d8c2ad06989e37034a52e0af13bc29
Greg <gskril@gmail.com>
2022-12-14 13:06:40 -0500

    Clean up _app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,43 @@
+import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+import PlausibleProvider from 'next-plausible'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { mainnet, goerli } from 'wagmi/chains'
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+
+import '../styles/globals.css'
+
+const { chains, provider } = configureChains(
+  [mainnet, goerli],
+  [infuraProvider({ apiKey: '' }), publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'ENS Fairy',
+  chains,
+})
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <PlausibleProvider domain={'ensfairy.xyz'} trackOutboundLinks>
+      <ThemeProvider theme={lightTheme}>
+        <ThorinGlobalStyles />
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider chains={chains}>
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </ThemeProvider>
+    </PlausibleProvider>
+  )
+}
